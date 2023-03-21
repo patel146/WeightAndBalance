025b273a5cb419c6f864e8726be1981b6709d614
Bhavya Patel <patelbhavya146@gmail.com>
2023-03-21 14:02:27 -0400

    CURRENT PLANE

--- actual_aircraft.py
+++ actual_aircraft.py
@@ -24,31 +24,31 @@ pilot = System("Pilot", 250*(1/0.9), 0.17, system_estimates, z_loc=4.23)
 # canopy = System("Canopy", 500, 0.1, system_estimates)
 
 # wing = System("Wing", estimates["Wing"], wing_pos, system_estimates)
-wing = System("Wing", 2732*(1/0.9), wing_pos, system_estimates,z_loc=1.06)
+wing = System("Wing", 2583*(1/0.9), wing_pos, system_estimates,z_loc=1.06)
 
 powerplant = System("Powerplant", 3075*(1/0.9), 0.84, system_estimates, z_loc=3.2)
 
-fuselage = System("Fuselage", 2200*(1/0.9), 0.45, system_estimates, z_loc=3.2)
+fuselage = System("Fuselage", 1700*(1/0.9), 0.45, system_estimates, z_loc=3.2)
 
-landing_gear = System("Landing Gear", 1211.54*(1/0.9), 0.4, system_estimates, z_loc=0.5)
+landing_gear = System("Landing Gear", 1250*(1/0.9), 0.4, system_estimates, z_loc=0.5)
 
 nacelle = System("Nacelle", estimates["Nacelle"], 0.65, system_estimates, z_loc=3.2)
 
 FCS = System("FCS", 850*(1/0.9), 0.38, system_estimates, z_loc=3.2)
 
-VT = System("VT", 412*(1/0.9), 0.88, system_estimates, z_loc=8.1)
+VT = System("VT", 361*(1/0.9), 0.88, system_estimates, z_loc=8.1)
 
-HT = System("HT", 299*(1/0.9), 0.92, system_estimates, z_loc=3.2)
+HT = System("HT", 235*(1/0.9), 0.92, system_estimates, z_loc=3.2)
 
-APU = System("APU", estimates["APU"], 0.3, system_estimates, z_loc=5.6)
+APU = System("APU", 110*(1/0.9), 0.3, system_estimates, z_loc=5.6)
 
 avionics = System("Avionics", estimates["Avionics"], 0.214, system_estimates, z_loc=3.27)
 
 fuel_systems = System("Fuel Systems", estimates["Fuel Systems"], 0.6, system_estimates, z_loc=3.2)
 
-elec_systems = System("Elec. Systems", estimates["Elec. Systems"], 0.5, system_estimates, z_loc=3.2)
+elec_systems = System("Elec. Systems", 300*(1/0.9), 0.5, system_estimates, z_loc=3.2)
 
-paint = System("Paint", estimates["Paint"], 0.4, system_estimates, z_loc=3.2)
+paint = System("Paint", 85*2*(1/0.9), 0.4, system_estimates, z_loc=3.2)
 
 # man_var = System("Man. Var.", estimates["Manu. Variation"], 0.5, system_estimates, z_loc=3.2)
 
@@ -58,4 +58,4 @@ furnishings = System("Furnishings", estimates["Furnishings"], 0.2, system_estima
 
 O2_system = System("O2 System", estimates["O2 System"], 0.24, system_estimates, z_loc=3.2)  # 29.5 lbf
 
-armour = System('Pilot Armour', 850, 0.17, system_estimates, z_loc=4.3)
+armour = System('Pilot Armour', 525*(1/0.9), 0.17, system_estimates, z_loc=4.3)
